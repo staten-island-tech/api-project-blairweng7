c958f40db1243b3c21ae9bcb8c965c1877edce20
blairweng7 <144455825+blairweng7@users.noreply.github.com>
2023-11-30 09:24:51 -0500

    sdafea

---
+++ notes.js
@@ -0,0 +1,31 @@
+//go get Lavian basketball data
+//show Lavian basketball data
+
+/* function greet(name) {
+const greetPromise = new Promise(function(resolve, reject){
+    resolve(`Hello ${name}`);
+});
+return greetPromise;
+}
+const Aaron = greet("Aaron")
+console.log(Aaron)
+//handle the promise
+Aaron.then((result)=> {
+    console.log(result);
+}); 
+*/
+
+//REST API
+const URL = `https//api.quotable.io/random`
+
+async function getData(URL) {
+    try {
+        const response = await fetch(URL);
+        console.log(response);
+        const data = await response.json();
+        console.log(data)
+        document.querySelector("h1").textContent == data.content;
+        document.querySelector("h2").textContent == data.author;
+    } catch (error) {}
+}
+getData(URL);
