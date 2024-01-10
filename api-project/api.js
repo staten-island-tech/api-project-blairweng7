5ea4c72ebfbd57abf24f6bf9e2a3dd466061f9c6
blairweng7 <144455825+blairweng7@users.noreply.github.com>
2024-01-10 09:23:47 -0500

    saf e;aiofna

--- api-project/api.js
+++ api-project/api.js
@@ -36,6 +36,7 @@ function addCards (arr){
       <h5 class="description" > ${items.description}</h5>
       <h5 class="dob"> ${items.date_of_birth_used}</h5>
       <h5 class="gender"> ${items.sex}</h5>
+      <h5 class ="uid"> ${items.uid}</h5>
     </div>
       `
   );
@@ -53,25 +54,40 @@ function addCards (arr){
         alert(message);
     }
 
-  function search(){
-    DOMSelectors.form.addEventListener("submit", async function(event){
-      event.preventDefault();
-      const searchInput = DOMSelectors.input.value;
-      if(searchInput){
-        const url = `${API} + ${searchInput.toLowerCase()}`;
-          try{
-            await getData(url);
-            clearFields();
-            addCards(url);
-          }catch(error){
-            console.log(error);
-            if (error.message === "Error");
-        errorMessage('Person not Found. Please try again.')
-          }
+const URL2 = "https://api.fbi.gov/";
+function oneCard(){
+  const inputName = DOMSelectors.input.value();
+  let URL3 = `https://api.fbi.gov/${inputName}`;
+  console.log(URL3)
+  addCards(URL3)
+}
+
+DOMSelectors.submit.addEventListener("click", function(){
+  event.preventDefault();
+  clearCards();
+  oneCard();
+  clearFields();
+})
+
+  // function search(){
+  //   DOMSelectors.form.addEventListener("submit", async function(event){
+  //     event.preventDefault();
+  //     const searchInput = DOMSelectors.input.value;
+  //     if(searchInput){
+  //       const url = `${API} + ${searchInput.toLowerCase()}`;
+  //         try{
+  //           await getData(url);
+  //           clearFields();
+  //           addCards(url);
+  //         }catch(error){
+  //           console.log(error);
+  //           if (error.message === "Error");
+  //       errorMessage('Person not Found. Please try again.')
+  //         }
         
-        clearCards();
-    }  
-    });
-  }
-  search()
+  //       clearCards();
+  //   }  
+  //   });
+  // }
+  // search()
   
--- api-project/dom.js
+++ api-project/dom.js
@@ -2,5 +2,6 @@ const DOMSelectors ={
     container: document.getElementById("container"),
     input: document.getElementById("People"),
     form: document.getElementById("form"),
+    submit: document.getElementById("submit")
 };  
 export { DOMSelectors };
